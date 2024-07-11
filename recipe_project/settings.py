e9c44b2be3b54200651a61ab9aae896a42301461
NozomiS0129 <129555124+NozomiS0129@users.noreply.github.com>
2024-07-10 23:22:03 -0400

    delete unnecessary line

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-import sys
 import dj_database_url
 from pathlib import Path
 
@@ -47,7 +46,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "crispy_forms",
     # recipe_project related apps
     "recipes",
     "users",
